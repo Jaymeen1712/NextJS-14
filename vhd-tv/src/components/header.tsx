38712d77446594a4b152a685cbfe36c398de929a
Jaymeen1712 <jaymeensonara@gmail.com>
2024-01-23 18:44:52 +0530

    Changes in header

--- vhd-tv/src/components/header.tsx
+++ vhd-tv/src/components/header.tsx
@@ -32,19 +32,21 @@ const Header = () => {
 
   return (
     <Navbar
-      className={`m-0 bg-neutral-950 shadow ${
+      className={`hello m-0 bg-neutral-950 shadow ${
         HEADER_TRANSPARENT.includes(selectedMenuItem.toLowerCase()) &&
-        "bg-0 absolute left-0 right-0 top-0 w-full"
+        "bg-0 absolute left-0 right-0 top-0"
       }`}
       height={"90px"}
+      position="static"
+      maxWidth="2xl"
     >
-      <NavbarBrand>
+      <NavbarBrand className="justify-center">
         <Link href="/">
           <Logo />
         </Link>
       </NavbarBrand>
 
-      <NavbarContent justify="end" className="">
+      <NavbarContent justify="start" className="gap-12">
         <NavbarItem>
           <Input
             size="sm"
@@ -56,13 +58,16 @@ const Header = () => {
             // classNames={{
             //   label: "text-white",
             //   input: ["bg-neutral-700"],
+            //   mainWrapper: ["bg-neutral-700"]
             // }}
-            className="min-w-[550px]"
+            className="min-w-[750px]"
           />
         </NavbarItem>
 
         {/* Menu Items */}
-        <div className="mx-10 flex flex-row space-x-10">{menuItems}</div>
+        <NavbarContent justify="end">
+          <div className="mx-10 flex flex-row space-x-12">{menuItems}</div>
+        </NavbarContent>
       </NavbarContent>
     </Navbar>
   );
